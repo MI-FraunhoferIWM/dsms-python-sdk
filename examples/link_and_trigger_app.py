af75ca473d12bebaa1e9dfc4f9efdb9d9422b1ad
Matthias Büschelberger <matthias.bueschelberger@iwm.fraunhofer.de>
2024-04-04 12:49:50 +0200

    update app example

--- examples/link_and_trigger_app.py
+++ examples/link_and_trigger_app.py
@@ -1,10 +1,12 @@
-from dsms import DSMS, KItem
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+from dsms import DSMS, KItem
 
 # set file paths
 env = os.path.join("..", ".env")
-file = os.path.join("..", "tensile_test.txt")
+file = "tensile_test.txt"
 
 # start session
 load_dotenv()
@@ -12,31 +14,36 @@ dsms = DSMS()
 
 # find appropiate app
 app_name = [
-    app.filename 
-    for app in dsms.apps 
-    if "csv" in app.filename 
+    app.filename
+    for app in dsms.apps
+    if "csv" in app.filename
     and "tensile" in app.filename
     and "test" in app.filename
 ]
 
 # configure app
 app = {
-        "title": "CSV Tensile test RDF Pipeline",
-        "description" : "Pipeline for CSV Tensile test data into RDF Graph",
-        "executable" : app_name.pop(),
-        "additional_properties": {
-            "triggerUponUpload": True,
-            "triggerUponUploadFileExtensions": ["." + file.split(".")[-1]]
-        }
-    }
+    "title": "CSV Tensile test RDF Pipeline",
+    "description": "Pipeline for CSV Tensile test data into RDF Graph",
+    "executable": app_name.pop(),
+    "additional_properties": {
+        "triggerUponUpload": True,
+        "triggerUponUploadFileExtensions": ["." + file.split(".")[-1]],
+    },
+}
+
+annotation = {
+    "iri": "https://w3id.org/steel/ProcessOntology/TensileTest",
+    "name": "Tensile test",
+    "namespace": "https://w3id.org/steel/ProcessOntology",
+}
 
 # make kitem
 item = KItem(
     name="DX56_A_FZ2_WR00_01_b",
     ktype_id=dsms.ktypes.Dataset,
-    kitem_apps=[
-        app
-    ]
+    kitem_apps=[app],
+    annotations=[annotation],
 )
 
 # commit the item
