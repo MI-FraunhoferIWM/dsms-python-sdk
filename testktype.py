d8d2e671a8285556962a7339bcc5b62318355810
Arjun Gopalakrishnan <arjun.gopalakrishnan@iwm.fraunhofer.de>
2024-09-24 11:42:30 +0200

    Example for ktype usage has been added.

--- testktype.py
+++ testktype.py
@@ -2,18 +2,18 @@ from dsms import DSMS, KItem, KType
 
 dsms = DSMS(env=".env")
 
-dsms.kitems
 # type = dsms.context.ktypes()
-# type = KType( 
-#         id='testtype20',
-#         name='newtype15'
-# )
-# dsms.commit()
+type = KType( 
+        id='batch',
+        name='Batch'
+)
+dsms.commit()
+print(type)
 # for i in range(15):
 #         if(i!=0):
 #                 ktype=dsms.context.ktypes['testtype'+str(i+1)]
 #                 print(ktype)
 #                 del dsms[ktype]
 #                 dsms.commit()
-for ktype in dsms.context.ktypes:
-        print(ktype)
+# for ktype in dsms.context.ktypes:
+#         print(ktype)
